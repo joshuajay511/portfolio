39fa376f091bdcb9f3057bbb74fe46ab612974ba
joshuajay511 <81710843+joshuajay511@users.noreply.github.com>
2022-07-28 10:13:23 -0500

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,73 @@
+
+// Mobile Nav //
+
+const hamburger = document.querySelector(".hamburger");
+const dropdown = document.querySelector(".dropdown");
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active");
+    dropdown.classList.toggle("active");
+});
+
+
+// Footer Dropdown //
+
+const accordion = document.getElementsByClassName("box");
+
+for (i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle("active");
+    })
+}
+
+
+// Slider //
+
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const dotsNav = document.querySelector('.carousel-nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+const interval = setInterval(function() {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+}, 5000);
+
+
+dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+});
+
